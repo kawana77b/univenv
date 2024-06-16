a140be13e6408d2e6f9dc32da1fa8043fdbcb63b
shimarisu_121 <73901165+kawana77b@users.noreply.github.com>
2024-06-16 23:25:17 +0900

    fix: main version

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"github.com/kawana77b/univenv/cmd"
 )
 
-var Version string = "0.0.1"
+var version string = "0.0.1"
 
 //go:embed template
 var fs embed.FS
 
 func main() {
 	cmd.Fs = fs
-	cmd.Version = Version
+	cmd.Version = version
 	cmd.Execute()
 }
